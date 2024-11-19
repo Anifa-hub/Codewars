e2beaefcaa7fd73c5e8168a7022b8e7bc53d44dc
anifa <anifabazubagira@gmail.com>
2024-11-19 14:44:49 +0200

    this contains new way you can remove duplicate

--- duplicate.js
+++ duplicate.js
@@ -1,3 +1,9 @@
-let arr= [2,2,4,5,6,]
+/*let arr= [2,2,4,5,6,]
 let duplicate = arr.filter((an,id) => arr.indexOf(an) === id);
-console.log(duplicate)
+console.log(duplicate)*/
+//other way
+function arr (number){
+    return[...new Set(number)]
+    
+}
+console.log(arr([1,2,2,4,5,2]))
