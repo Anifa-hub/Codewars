d17921cbd4fb596a987bb81486a4f5718c27f457
anifa <anifabazubagira@gmail.com>
2024-11-19 13:43:53 +0200

    this rertuns the number that is divisible by given diviso

--- findDivisor.js
+++ findDivisor.js
@@ -1,11 +1,15 @@
-function shortCut (number,b){
-    let remain=[]
-         if(number[0]%b ===0 || number[1]%b ===0 || number[2]%b ===0 || number[3]%b ===0){
-             remain.push(number) 
-         }else{
-             console.log('')
-         }
-     return remain
-     
- }
- console.log(shortCut([2,4,5,6],2))
+function divisibleBy(numbers, divisor){
+  
+    let remain =[]
+   for (let i=0; i<numbers.length; i++){
+   
+        if(numbers[i]%divisor===0){
+            remain.push(numbers[i])
+        }else{
+            console.log('')
+        }
+        
+}
+ return remain  
+}
+console.log(divisibleBy([2,4,5,6,7],2))
