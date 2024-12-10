a53bbcfdb54645ec59b79b4576bc4c2f6c16e70e
anifa <anifabazubagira@gmail.com>
2024-12-10 14:55:06 +0200

    return Hello, Name or World!

---
+++ helloNameOrworld!.js
@@ -0,0 +1,12 @@
+
+
+function hello(name) {
+
+  if(name === '' || !name){
+    return 'Hello, World!'
+      
+  }else{
+return `Hello, ${name.charAt(0).toUpperCase()+name.slice(1).toLowerCase()}!`
+  
+}}
+console.log(hello())
