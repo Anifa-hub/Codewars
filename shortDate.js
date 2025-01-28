31248d4f6ad403949eb327304dacf8daf03d0920
anifa <anifabazubagira@gmail.com>
2025-01-28 10:47:39 +0200

    Write a function that takes the website date/time in its original string format and returns the shortened format

---
+++ shortDate.js
@@ -0,0 +1,11 @@
+function shortenToDate(longDate) {
+    // let time = longDate.split(',')[0]
+    // return time
+    
+    
+  let [x , d]=longDate.split(',')
+    return x
+  
+
+}
+console.log(shortenToDate("Friday May 2, 7pm"))
