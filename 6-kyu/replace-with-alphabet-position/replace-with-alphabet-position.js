99d539f4d42e859980d1430b991932da67175906
Anifa <anifabazubagira@gmail.com>
2025-02-25 15:54:30 +0200

    Update solution - CodeHub

--- 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.js
+++ 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.js
@@ -1,3 +1,4 @@
+ 
 function alphabetPosition(text) {
-  return text.split('').filter(v=>/[a-zA-Z]/).map(v=>v.toLowerCase().charCodeAt(0)-96).join(' ');
+  return text.split('').filter(v=>/[a-zA-Z]/).map(v=>v.toLowerCase().charCodeAt(0)-26).join(' ');
 }
