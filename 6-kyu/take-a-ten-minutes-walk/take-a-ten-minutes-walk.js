74cf3e7bad4c49c6d437b5e792ad09c97a37fbe3
Anifa <anifabazubagira@gmail.com>
2025-02-26 10:19:44 +0200

    Update solution - CodeHub

--- 6-kyu/take-a-ten-minutes-walk/take-a-ten-minutes-walk.js
+++ 6-kyu/take-a-ten-minutes-walk/take-a-ten-minutes-walk.js
@@ -1,8 +1,12 @@
  
 function isValidWalk(walk) {
-  if(walk.length ==10){
-    return true
-  }else{
-    return false
-  }
+  let ns = 0, we = 0; 
+    for (let dir of walk) { 
+      if (dir == 'n') ns += 1; 
+      if (dir == 's') ns -= 1; 
+      if (dir == 'w') we += 1; 
+      if (dir == 'e') we -= 1; 
+    } 
+​
+    return walk.length == 10 && ns === 0 && we === 0; 
 }
