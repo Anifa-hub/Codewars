8f2f2acac3f69c9f82240646ab7874399efe1154
Anifa <anifabazubagira@gmail.com>
2025-03-07 10:42:02 +0200

    Update solution - CodeHub

--- 7-kyu/get-the-middle-character/get-the-middle-character.js
+++ 7-kyu/get-the-middle-character/get-the-middle-character.js
@@ -1,5 +1,10 @@
- 
 function getMiddle(s) {
   let middle = s.length/2
-    return s%2 ===1? s.charAt(middle):s.slice(middle-1,middle+1)
+  if(s.length %2 == 1){
+      return  s.charAt(middle)
+  }else{
+      return s.slice(middle-1, middle+1)
+  }
+  
+  
 }
