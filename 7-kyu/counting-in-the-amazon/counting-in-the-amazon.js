70f9781af8e2ea211d4d1c6b69887cf99bc9f61f
Anifa <anifabazubagira@gmail.com>
2025-04-01 10:28:24 +0200

    Add solution - CodeHub

---
+++ 7-kyu/counting-in-the-amazon/counting-in-the-amazon.js
@@ -0,0 +1,21 @@
+function countArara(n) {
+    if(n===1){
+      return 'anane'
+    }else if(n===2){
+      return 'adak'
+    }else if(n===3){
+      return 'adak anane'
+    }else if (n===4){
+      return 'adak adak'
+        }else if(n===5){
+          return 'adak adak anane'
+        }else if(n===6){
+          return 'adak adak adak'
+        }else if(n===7){
+          return 'adak adak adak anane'
+        }else if(n===8){
+          return 'adak adak adak adak'
+        }
+      
+  
+}
