e846ab52ddc4b19e16769e40265d21ae5b6fccde
Anifa <anifabazubagira@gmail.com>
2025-04-03 10:25:48 +0200

    Update solution - CodeHub

--- 7-kyu/initialize-my-name/initialize-my-name.js
+++ 7-kyu/initialize-my-name/initialize-my-name.js
@@ -1,7 +1,7 @@
  
 function initializeNames(name){
   let arr = name.split(' ');
-   for (let i = 0; i < arr.length ; i++) 
+   for (let i = 1; i < arr.length - 1; i++) 
        arr[i] = arr[i].charAt(0) + '.';
    return arr.join(' ');
 }
