d91ff136a0b91dd6a988ddc11898b4e56507305c
Anifa <anifabazubagira@gmail.com>
2025-04-23 10:09:20 +0200

    Add solution - CodeHub

---
+++ 6-kyu/binary-to-text-ascii-conversion/binary-to-text-ascii-conversion.js
@@ -0,0 +1,18 @@
+ 
+function binaryToString(binary) {
+  if (binary.length % 8 !== 0) {
+    return "Binary string length is not a multiple of 8.";
+  }
+​
+  let text = "";
+  for (let i = 0; i < binary.length; i += 8) {
+    const chunk = binary.slice(i, i + 8);
+    const decimal = parseInt(chunk, 2);
+​
+    if (decimal < 0 || decimal > 127) {
+      return "Invalid ASCII code found.";
+    }
+    text += String.fromCharCode(decimal);
+  }
+  return text;
+}
