1e8aa579f7653c10045fc7715bafafb6feb67f5c
Anifa <anifabazubagira@gmail.com>
2025-04-25 10:00:33 +0200

    Add solution - CodeHub

---
+++ 6-kyu/even-or-odd-accessor/even-or-odd-accessor.js
@@ -0,0 +1,14 @@
+ 
+function evenOrOdd(n) {
+  if( n.isArray && n[0]%2 ===0){
+    return "Even"
+  }else {
+    return "Odd"
+  }
+  if(n%2===0) {
+    return "Even"
+  }else {
+    return "Odd"
+  }
+  
+}
