0b021046a8dfb7088604bd54b61fe4835fdb2572
Anifa <anifabazubagira@gmail.com>
2025-04-30 10:15:20 +0200

    Update solution - CodeHub

--- 6-kyu/array-dot-isarray/array-dot-isarray.js
+++ 6-kyu/array-dot-isarray/array-dot-isarray.js
@@ -1,5 +1,5 @@
  
 function isArray(arr) {
   // This is what we would like to do, but it's not allowed.
-  return Array.isArray(arr);
+  return Object.prototype.toString.call(arr) === '[object Array]'
 }
