2b1a0b25feb3b327686ab2ff8697a268223f09dc
Anifa <anifabazubagira@gmail.com>
2025-06-09 11:12:36 +0200

    Add solution - CodeHub

---
+++ 5-kyu/human-readable-time/human-readable-time.js
@@ -0,0 +1,7 @@
+ 
+function humanReadable(seconds) {
+  const hours = "0"+Math.floor(seconds / 3600);
+     const minutes = "0"+Math.floor((seconds % 3600) / 60);
+     const second = "0"+seconds % 60
+  return hours.slice(-2) + ":" + minutes.slice(-2) + ":" + second.slice(-2);
+}
