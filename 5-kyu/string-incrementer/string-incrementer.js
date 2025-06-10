5d093738902511c3c2906dbe21d776f429e38a06
Anifa <anifabazubagira@gmail.com>
2025-06-10 14:41:54 +0200

    Add solution - CodeHub

---
+++ 5-kyu/string-incrementer/string-incrementer.js
@@ -0,0 +1,16 @@
+function incrementString(strng) {
+  if (isNaN(Number(strng[strng.length - 1]))) return strng + 1;
+  let word = "", len
+  for (let i = strng.length - 1; i >= 0; i--) {
+    if (!isNaN(+strng[i])) {
+      word = strng[i] + word; 
+      strng = strng.slice(0,i)
+    } else {
+      break
+    }
+  }
+  len = word.length
+  word = (+word + 1) + ''
+  if(word.length < len) for(let i = 0; i <= len - word.length; i++) word = '0' + word
+  return strng + word;
+}
