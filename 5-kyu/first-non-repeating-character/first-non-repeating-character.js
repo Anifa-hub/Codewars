5a1608a1985d95dfa563a6dd06b5c24081ddfb1a
Anifa <anifabazubagira@gmail.com>
2025-06-11 10:20:02 +0200

    Update solution - CodeHub

--- 5-kyu/first-non-repeating-character/first-non-repeating-character.js
+++ 5-kyu/first-non-repeating-character/first-non-repeating-character.js
@@ -1,9 +1,11 @@
+ 
 function firstNonRepeatingLetter(s) {
   let letters = s.split('')
   let room =[]
   for(let letter of letters){
     if(letters.indexOf(letter)== letters.lastIndexOf(letter)){
       room.push(letter)
+      break
     }
        ""
   }
