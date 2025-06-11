701b2a043b3bf6adedee20d16c60c2754d097c9e
Anifa <anifabazubagira@gmail.com>
2025-06-11 10:17:39 +0200

    Add solution - CodeHub

---
+++ 5-kyu/first-non-repeating-character/first-non-repeating-character.js
@@ -0,0 +1,8 @@
+function firstNonRepeatingLetter(s) {
+  let letters = s.split('')
+  for(let letter of letters){
+    if(letters.indexOf(letter)== letters.lastIndexOf(letter)){
+      return letter
+    }
+  }
+}
