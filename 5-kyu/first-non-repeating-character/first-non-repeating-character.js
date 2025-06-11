b79363abb992c84c29e4f4772f53b6665fb9ee97
Anifa <anifabazubagira@gmail.com>
2025-06-11 10:25:43 +0200

    Update solution - CodeHub

--- 5-kyu/first-non-repeating-character/first-non-repeating-character.js
+++ 5-kyu/first-non-repeating-character/first-non-repeating-character.js
@@ -2,9 +2,7 @@
 function firstNonRepeatingLetter(s) {
   let letters = s.split('')
   for(let letter of letters){
-    if(letters.indexOf(letter) === letters.lastIndexOf(letter)){
-      return letter
-    }
+    letters.indexOf(letter) === letters.lastIndexOf(letter))? letter : ""
   }
 }
 console.log(firstNonRepeatingLetter("andinda"))
