297262d788cd35ad3f9047fd62b1135a4672feb8
Anifa <anifabazubagira@gmail.com>
2025-06-23 14:55:07 +0200

    Add solution - CodeHub

---
+++ 5-kyu/extract-the-domain-name-from-a-url-1/extract-the-domain-name-from-a-url-1.js
@@ -0,0 +1,15 @@
+function domainName(url){
+  let newurl
+  if(url.includes('//') && url.includes('www')){
+      newurl= url.split('www').join('').split('.').slice(1,2).join('')
+  }
+  else if(url.includes('//')){ newurl = url.split('//').slice(1,2).join('').split('.').slice(0,1).join('')}
+  else if(!url.includes('//') && !url.includes('www')){
+      newurl = url.split('.').slice(0,1).join('')
+  }
+  else{ newurl = url.split('.').slice(1,2).join('')
+  }
+​
+  return newurl
+}
+console.log(domainName("www.xakep.ru"))
