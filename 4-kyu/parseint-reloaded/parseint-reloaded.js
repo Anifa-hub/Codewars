c5b803c07a7165044b8eee52124272a57ac311a0
Anifa <anifabazubagira@gmail.com>
2025-07-01 12:24:07 +0200

    Update solution - CodeHub

--- 4-kyu/parseint-reloaded/parseint-reloaded.js
+++ 4-kyu/parseint-reloaded/parseint-reloaded.js
@@ -1,4 +1,16 @@
- 
+const wordReps = {
+  ninety: 90,    nineteen: 19,    nine: 9,
+  eighty: 80,    eighteen: 18,    eight: 8,
+  seventy: 70,   seventeen: 17,   seven: 7,
+  sixty: 60,     sixteen: 16,     six: 6,
+  fifty: 50,     fifteen: 15,     five: 5,
+  forty: 40,     fourteen: 14,    four: 4,
+  thirty: 30,    thirteen: 13,    three: 3,
+  twenty: 20,    twelve: 12,      two: 2,
+  ten: 10,       eleven: 11,      one: 1,
+                                  zero: 0
+};
+​
 function parseInt(string) {
   string = string.toLowerCase();
   string = string.replace(/(\sand\s|[-])/g, " ");
