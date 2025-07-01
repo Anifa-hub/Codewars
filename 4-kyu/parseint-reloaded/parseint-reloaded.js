3ae27c483962d31b4d21780165d46ac8f45473c5
Anifa <anifabazubagira@gmail.com>
2025-07-01 12:19:54 +0200

    Update solution - CodeHub

--- 4-kyu/parseint-reloaded/parseint-reloaded.js
+++ 4-kyu/parseint-reloaded/parseint-reloaded.js
@@ -1,3 +1,4 @@
+ 
 function parseInt(string) {
   let map = {"zero":0,"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9,"ten":10,"eleven":11,"twelve":12,"thirteen":13,"fourteen":14,"fifteen":15,"sixteen":16,"seventeen":17,"eigthteen":18,"nineteen":19,
     "twenty":20, "thirty":30, "forty":40, "fifty":50, "sixty":60, "seventy":70, "eighty":80, "ninety":90,};
@@ -10,4 +11,5 @@ function parseInt(string) {
       return map[nArr[0]] + map[nArr[1]];
     }
     return map[string];
+    }
   
