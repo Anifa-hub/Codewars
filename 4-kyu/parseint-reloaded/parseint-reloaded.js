6a36cb809ac901281928665ba33bba8721cf1223
Anifa <anifabazubagira@gmail.com>
2025-07-01 12:19:36 +0200

    Add solution - CodeHub

---
+++ 4-kyu/parseint-reloaded/parseint-reloaded.js
@@ -0,0 +1,14 @@
+ 
+function parseInt(string) {
+  let map = {"zero":0,"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9,"ten":10,"eleven":11,"twelve":12,"thirteen":13,"fourteen":14,"fifteen":15,"sixteen":16,"seventeen":17,"eigthteen":18,"nineteen":19,
+    "twenty":20, "thirty":30, "forty":40, "fifty":50, "sixty":60, "seventy":70, "eighty":80, "ninety":90,};
+  let multiMap = {"hundred":100, "thousand":1000, "million":1000000};
+  let result = 0, result2 = 0, multiply = 0;
+  
+  function getNumber(string) {
+    let nArr = string.split("-");
+    if (nArr.length > 1) {
+      return map[nArr[0]] + map[nArr[1]];
+    }
+    return map[string];
+  
