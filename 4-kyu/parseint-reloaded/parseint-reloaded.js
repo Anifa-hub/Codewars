a32367c1ecbe4f1023a5034cf8d92ddf1111ae8f
Anifa <anifabazubagira@gmail.com>
2025-07-01 12:23:33 +0200

    Update solution - CodeHub

--- 4-kyu/parseint-reloaded/parseint-reloaded.js
+++ 4-kyu/parseint-reloaded/parseint-reloaded.js
@@ -1,26 +1,26 @@
  
-    var ref = { one:1, two:2, three:3, four:4, five:5, six:6, seven:7, eight:8, nine:9, ten:10, eleven:11, twelve:12, thirteen:13, fourteen:14, fifteen:15, sixteen:16, seventeen:17, eighteen:18, nineteen:19, twenty:20, thirty: 30, forty: 40, fifty: 50, sixty: 60, seventy: 70, eighty: 80, ninety:90 },
-        mult = { hundred: 100, thousand: 1000, million: 1000000 },
-        strNums = numbersInString.split(' ').reverse(),
-        number = 0,
-        multiplier = 1;
+function parseInt(string) {
+  string = string.toLowerCase();
+  string = string.replace(/(\sand\s|[-])/g, " ");
+  string = string.replace(/[,]/g, "");
 ​
-    for(i in strNums){
-        if( mult[strNums[i]] != undefined ) {
-            if(mult[strNums[i]]==100) {
-                multiplier*=mult[strNums[i]]
-            }else{
-                multiplier=mult[strNums[i]]
-            }
-        } else {
-            if (!isNaN(parseFloat(strNums[i]))) {
-                number += parseFloat(strNums[i]) * multiplier;
-            } else {
-                var nums = strNums[i].split('-');
-                number += ((ref[nums[0]]||0) + (ref[nums[1]]||0)) * multiplier;
-            }
-        }
-    }
-    return number;
+  if (string.includes("million")) return 1000000;
+​
+  const strArr = string.split(" ");
+​
+  const parsed = strArr => {
+    return strArr.reduce((acc, word, i, words) => {
+      switch (word) {
+        case "thousand":
+          return (acc = acc * 1000 + parsed(words.splice(i + 1)));
+        case "hundred":
+          return (acc *= 100);
+        default:
+          return (acc += wordReps[word]);
+      }
+    }, 0);
+  };
+​
+  return parsed(strArr);
 }
   
