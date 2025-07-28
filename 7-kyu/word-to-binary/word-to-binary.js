e7ca00d9328013c50fa2b3f9165fe45d863eaa88
Anifa <anifabazubagira@gmail.com>
2025-07-28 10:44:39 +0200

    Update solution - CodeHub

--- 7-kyu/word-to-binary/word-to-binary.js
+++ 7-kyu/word-to-binary/word-to-binary.js
@@ -1,6 +1,9 @@
 ​
-function wordToBin(str){
-  
-  return str.split('').map(x=>x.toString(2));
+function wordToBin(str) {
+  return str.split('').map(char => {
+    const asciiValue = char.charCodeAt(0); // Get the ASCII value of the character
+    const binaryString = asciiValue.toString(2); // Convert the ASCII value to a binary string
+    return binaryString.padStart(8, '0'); // Pad with leading zeros to make it 8 digits
+  });
 }
 ​
