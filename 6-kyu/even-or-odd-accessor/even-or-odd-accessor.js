7024fcd17228194857d4f0ff3fad960bd613d92c
Anifa <anifabazubagira@gmail.com>
2025-07-31 10:47:15 +0200

    Update solution - CodeHub

--- 6-kyu/even-or-odd-accessor/even-or-odd-accessor.js
+++ 6-kyu/even-or-odd-accessor/even-or-odd-accessor.js
@@ -1,6 +1,6 @@
  
 function evenOrOdd(n) {
-  if( Array.isArray(n) === true && [n]%2 ===0 || n % 2 === 0){
+  if( Array.isArray(n) === true && [n].map((x)=>x%2 ===0) || n % 2 === 0){
     return "Even"
   }
   if(Array.isArray(n) === false && [n] % 2 !== 0 || n % 2 !== 0) {
