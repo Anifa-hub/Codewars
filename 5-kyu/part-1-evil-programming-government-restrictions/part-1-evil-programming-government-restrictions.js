21a2163bd5c26ba0b313b451a0b5dd04de8f0dce
Anifa <anifabazubagira@gmail.com>
2025-08-04 10:54:59 +0200

    Update solution - CodeHub

--- 5-kyu/part-1-evil-programming-government-restrictions/part-1-evil-programming-government-restrictions.js
+++ 5-kyu/part-1-evil-programming-government-restrictions/part-1-evil-programming-government-restrictions.js
@@ -1,6 +1,5 @@
- 
-let anarchy = function () {
- const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  let ans= letters.slice(0, 3).join(', ');
-  return 'I can write numbers' +' ' + ans
-}
+ const one = 'a'.length;
+  const two = 'ab'.length;
+  const three = 'abc'.length;
+  
+  return `I can write numbers like, ${one}, ${two}, ${three}.`;
