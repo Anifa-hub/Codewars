1e956ad2520a1205392412eafa14e0d2715f93c1
Anifa <anifabazubagira@gmail.com>
2025-08-04 10:55:20 +0200

    Update solution - CodeHub

--- 5-kyu/part-1-evil-programming-government-restrictions/part-1-evil-programming-government-restrictions.js
+++ 5-kyu/part-1-evil-programming-government-restrictions/part-1-evil-programming-government-restrictions.js
@@ -1,5 +1,8 @@
- const one = 'a'.length;
+let anarchy = function () {
+  const one = 'a'.length;
   const two = 'ab'.length;
   const three = 'abc'.length;
   
   return `I can write numbers like, ${one}, ${two}, ${three}.`;
+};
+​
