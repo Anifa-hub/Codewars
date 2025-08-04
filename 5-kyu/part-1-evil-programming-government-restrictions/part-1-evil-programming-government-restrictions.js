acb5e1c9aa743c2a5ab4ba356e0ba7953ac21165
Anifa <anifabazubagira@gmail.com>
2025-08-04 10:52:39 +0200

    Update solution - CodeHub

--- 5-kyu/part-1-evil-programming-government-restrictions/part-1-evil-programming-government-restrictions.js
+++ 5-kyu/part-1-evil-programming-government-restrictions/part-1-evil-programming-government-restrictions.js
@@ -1,4 +1,5 @@
 let anarchy = function () {
  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  return letters.slice(0, 3).join(', ');
+  let ans= letters.slice(0, 3).join(', ');
+  return 'I can write numbers' + ans
 }
