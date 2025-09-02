a40ca19f4de295da561e065e37c5a0f55d48642d
Anifa <anifabazubagira@gmail.com>
2025-09-02 11:59:16 +0200

    Update solution - CodeHub

--- 6-kyu/sum-two-arrays/sum-two-arrays.js
+++ 6-kyu/sum-two-arrays/sum-two-arrays.js
@@ -2,7 +2,6 @@
 function addArrays(array1, array2) {
   let arrayToNumber1 = parseInt(array1.join(''));
   let arrayToNumber2 = parseInt(array2.join(''));
-  return String(arrayToNumber1 + arrayToNumber2).split('').map((x)=>
-  +(x)
+  return String(arrayToNumber1 + arrayToNumber2).split('')
   )
 }
