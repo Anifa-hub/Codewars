71ade0c2580e3d4f29a4119f525547a5f4919a2c
Anifa <anifabazubagira@gmail.com>
2025-09-03 14:26:52 +0200

    Add solution - CodeHub

---
+++ 8-kyu/remove-first-and-last-character-part-two/remove-first-and-last-character-part-two.js
@@ -0,0 +1,14 @@
+ 
+function array(string) {
+  if (string === null || string === undefined || string.length === 0) {
+    return null; 
+  }
+​
+  const parts = string.split(',');
+​
+  if (parts.length <= 2) {
+    return null; 
+  }
+​
+  return parts.slice(1, -1).join(' ');
+}
