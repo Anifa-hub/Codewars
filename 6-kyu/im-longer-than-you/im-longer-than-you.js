959f9e694fe8a48bff2b3dc0911e4ca496b3933c
Anifa <anifabazubagira@gmail.com>
2025-09-17 11:33:41 +0200

    Update solution - CodeHub

--- 6-kyu/im-longer-than-you/im-longer-than-you.js
+++ 6-kyu/im-longer-than-you/im-longer-than-you.js
@@ -1,16 +1,12 @@
+ 
 function longer(s) {
-  // 1. Split the string into an array of words
-  return s.split(' ')
-    // 2. Sort the array using a custom comparison function
-    .sort((a, b) => {
-      // Primary sort: by word length (ascending)
+ return s.split(' ').sort((a, b) => {
       if (a.length !== b.length) {
         return a.length - b.length;
       }
-      // Secondary sort: alphabetical (case-sensitive)
-      return a.localeCompare(b);
-    })
-    // 3. Join the sorted array back into a string
-    .join(' ');
-}
+        return a.localeCompare(b)
+   return b.localeCompare(a)
+      }
+).join(' ')
+  }
 ​
