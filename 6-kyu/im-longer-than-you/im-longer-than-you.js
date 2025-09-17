ca52eab84f10d12bd97389fb07988964b976a8fe
Anifa <anifabazubagira@gmail.com>
2025-09-17 11:34:32 +0200

    Update solution - CodeHub

--- 6-kyu/im-longer-than-you/im-longer-than-you.js
+++ 6-kyu/im-longer-than-you/im-longer-than-you.js
@@ -1,12 +1,25 @@
- 
 function longer(s) {
- return s.split(' ').sort((a, b) => {
-      if (a.length !== b.length) {
-        return a.length - b.length;
+  return s.split(' ').sort((a, b) => {
+    // Sort by length in ascending order
+    if (a.length !== b.length) {
+      return a.length - b.length;
+    }
+    // If lengths are equal, sort alphabetically,
+    // ensuring lowercase comes after uppercase for the same letter.
+    // This custom comparison handles 'Hello' before 'hello'.
+    const minLength = Math.min(a.length, b.length);
+    for (let i = 0; i < minLength; i++) {
+      const charA = a.charCodeAt(i);
+      const charB = b.charCodeAt(i);
+​
+      if (charA !== charB) {
+        // Direct ASCII comparison works for 'Hello' < 'hello'
+        return charA - charB;
       }
-        return a.localeCompare(b)
-   return b.localeCompare(a)
-      }
-).join(' ')
-  }
+    }
+    // If one is a prefix of the other (e.g., "a" vs "aa"), shorter comes first
+    return a.length - b.length;
+  }).join(' ');
+}
+​
 ​
