558ae33785e99b6a8db1bf130daf9610dcd1ac5d
Anifa <anifabazubagira@gmail.com>
2025-09-17 11:37:54 +0200

    Update solution - CodeHub

--- 6-kyu/im-longer-than-you/im-longer-than-you.js
+++ 6-kyu/im-longer-than-you/im-longer-than-you.js
@@ -1,23 +1,4 @@
+ 
 function longer(s) {
-  return s.split(' ').sort((a, b) => {
-   
-    if (a.length !== b.length) {
-      return a.length - b.length;
-    }
- 
-    const minLength = Math.min(a.length, b.length);
-    for (let i = 0; i < minLength; i++) {
-      const charA = a.charCodeAt(i);
-      const charB = b.charCodeAt(i);
-​
-      if (charA !== charB) {
-      
-        return charA - charB;
-      }
-    }
-  
-    return a.length - b.length;
-  }).join(' ');
+return s.split(' ').sort().sort((a,b) => a.length - b.length).join(' ');
 }
-​
-​
