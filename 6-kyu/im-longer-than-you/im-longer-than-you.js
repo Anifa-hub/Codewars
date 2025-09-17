31463d323c1684b917f8bb57371d1e348b40f534
Anifa <anifabazubagira@gmail.com>
2025-09-17 11:28:42 +0200

    Update solution - CodeHub

--- 6-kyu/im-longer-than-you/im-longer-than-you.js
+++ 6-kyu/im-longer-than-you/im-longer-than-you.js
@@ -1,8 +1,9 @@
- 
 function longer(s) {
  return s.split(' ').sort((a, b) => {
       if (a.length !== b.length) {
         return a.length - b.length;
+      }else if(a.length === b.length){
+        return s.sort()
       }
 }).join(' ')
   }
