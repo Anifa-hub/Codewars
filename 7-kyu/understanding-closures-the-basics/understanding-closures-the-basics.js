eeac7a754ab3b98df1a81a19af83afc9293d10ca
Anifa <anifabazubagira@gmail.com>
2025-10-03 15:29:10 +0200

    Update solution - CodeHub

--- 7-kyu/understanding-closures-the-basics/understanding-closures-the-basics.js
+++ 7-kyu/understanding-closures-the-basics/understanding-closures-the-basics.js
@@ -1,11 +1,14 @@
-function buildFun(n){
-​
-  let res = []
-​
-  for (let i = 0; i< n; i++){
-    res.push(function(n){
-      console.log(i)
-    })
+function buildFun(N) {
+  var arr = [];
+  // Must use 'var' here as we rely on the IIFE for scoping
+  for (var i = 0; i < N; i++) {
+    // The IIFE creates a new scope and takes 'i' as an argument,
+    // storing it in the locally scoped variable 'j'.
+    (function(j) {
+      arr.push(function() {
+        return j;
+      });
+    })(i); // Immediately execute the function, passing 'i'
   }
-  return res
+  return arr;
 }
