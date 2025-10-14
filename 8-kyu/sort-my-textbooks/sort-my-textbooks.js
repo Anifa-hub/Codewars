4c894fbec5443ae06ed41c60e41b18fd149bde45
Anifa <anifabazubagira@gmail.com>
2025-10-14 08:43:30 -0700

    Update solution - CodeHub

--- 8-kyu/sort-my-textbooks/sort-my-textbooks.js
+++ 8-kyu/sort-my-textbooks/sort-my-textbooks.js
@@ -1,4 +1,5 @@
+ 
 function sorter(textbooks) {
 //   let ans = textbooks.join(' ').toLowerCase()
-  return textbooks.join(' ').toLowerCase().split('').sort();
+  return textbooks.join(' ').toLowerCase().split(' ').sort();
 }
