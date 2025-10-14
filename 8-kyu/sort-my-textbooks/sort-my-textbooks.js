b5fe65cabdad06c141ff75abca980e0504c53f05
Anifa <anifabazubagira@gmail.com>
2025-10-14 08:47:09 -0700

    Update solution - CodeHub

--- 8-kyu/sort-my-textbooks/sort-my-textbooks.js
+++ 8-kyu/sort-my-textbooks/sort-my-textbooks.js
@@ -1,2 +1,9 @@
  
-const sorter = (textbooks) => textbooks.sort((a, b) => (a.charCodeAt(0) !== b.charCodeAt(0) ? a.charCodeAt(0) - b.charCodeAt(0) : a.toLowerCase().localeCompare(b.toLowerCase())));
+const sorter = (textbooks) => textbooks.sort((a, b) => {
+    // Check if the first character's unicode value is lower (e.g., '$' is lower than '*')
+    if (a.charCodeAt(0) !== b.charCodeAt(0)) {
+        return a.charCodeAt(0) - b.charCodeAt(0);
+    }
+    // If the first char is the same, fall back to alphabetical
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+});
