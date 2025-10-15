f4e8ecb4dab43700e7c068756ddabbf38d819edd
Anifa <anifabazubagira@gmail.com>
2025-10-15 01:52:48 -0700

    Update solution - CodeHub

--- 8-kyu/find-the-position/find-the-position.java
+++ 8-kyu/find-the-position/find-the-position.java
@@ -3,11 +3,12 @@ public class Kata
 {
   public static String position(char alphabet)
   {
-    if(alphabet.equals("a")){return 1;}
-    else if(alphabet.equals("b")){return 2;}
-    else if(alphabet.equals("c")){return 3;}
-    else if(alphabet.equals("d")){return 4;}
-    else if(alphabet.equals("e")){return 5;}
-    else if(alphabet.equals("f")){return 6;}
+    if(alphabet=="a"){return 1;}
+    else if(alphabet=="b"){return 2;}
+    else if(alphabet=="c"){return 3;}
+    else if(alphabet=="d"){return 4;}
+    else if(alphabet=="e"){return 5;}
+    else if(alphabet=="f"){return 6;}
+    
   }
 }
