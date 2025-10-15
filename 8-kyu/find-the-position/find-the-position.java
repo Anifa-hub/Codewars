85da3d7eb00107f85d3650a98d404776872e642f
Anifa <anifabazubagira@gmail.com>
2025-10-15 01:56:00 -0700

    Update solution - CodeHub

--- 8-kyu/find-the-position/find-the-position.java
+++ 8-kyu/find-the-position/find-the-position.java
@@ -6,7 +6,9 @@ public class Kata
      if (alphabet >= 'a' && alphabet <= 'z') {
             int position = alphabet - 'a' + 1;
             return "Position of alphabet: " + position;
-        }
+        }else {
+        return "inpur should be lowercase";
+     }
     
   }
 }
