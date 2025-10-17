211c52258d37cb9a8b2457cd89faa8cb929f45ea
Anifa <anifabazubagira@gmail.com>
2025-10-17 00:31:57 -0700

    Add solution - CodeHub

---
+++ 8-kyu/a-needle-in-the-haystack/a-needle-in-the-haystack.java
@@ -0,0 +1,13 @@
+public class Kata {
+  public static String findNeedle(Object[] haystack) {
+    for(int i=0; i<haystack.length; i++){
+      if("needle".equals(haystack[i])){
+        return "found the needle at position " + i;
+        
+        }
+      }
+    return "There is no needle";
+    
+    }
+  }
+​
