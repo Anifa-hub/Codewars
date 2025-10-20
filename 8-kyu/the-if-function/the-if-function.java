5ab71e76c0536f92280d6f2522970614cf0b372c
Anifa <anifabazubagira@gmail.com>
2025-10-20 01:48:43 -0700

    Update solution - CodeHub

--- 8-kyu/the-if-function/the-if-function.java
+++ 8-kyu/the-if-function/the-if-function.java
@@ -2,10 +2,10 @@
 public class Kata {
   public static void _if(Boolean bool, Runnable func1, Runnable func2) {
    if (bool==true){
-     System.out.println(func1);
+     System.out.println(func1());
    }
     else{
-      System.out.println(func2);
+      System.out.println(func2());
     }
   }
 }
