f02c6657ecfa7d2bae8433d640ca85893115de76
Anifa <anifabazubagira@gmail.com>
2025-10-20 01:47:15 -0700

    Add solution - CodeHub

---
+++ 8-kyu/the-if-function/the-if-function.java
@@ -0,0 +1,8 @@
+public class Kata {
+  public static void _if(Boolean bool, Runnable func1, Runnable func2) {
+   if (bool==true){
+     return Runnable func1;
+   }
+    return Runnable func2;
+  }
+}
