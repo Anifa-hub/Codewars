a5fa899e508b159b0c54efec4ff857207c54ef92
Anifa <anifabazubagira@gmail.com>
2025-10-21 05:54:23 -0700

    Add solution - CodeHub

---
+++ 8-kyu/switch-it-up/switch-it-up.java
@@ -0,0 +1,33 @@
+ 
+public class Kata
+{
+  public static String switchItUp(int number)
+  {
+    switch (number){
+        case number==1;
+        return "One";
+        break;
+        case number==2;
+        return "Two";
+        break;
+        case number==3;
+        return "Three";
+        break;
+        case number==4;
+        return "Four";
+        break;
+        case number==5;
+        return "Five";
+        break;
+        case number==6;
+        return "Six";
+        break;
+        case number==7;
+        return "Seven";
+        break;
+        default:
+        return "Eight";
+        break;
+    }
+  }
+}
