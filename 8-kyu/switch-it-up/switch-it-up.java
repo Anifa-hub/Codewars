e2d50fa2b1d7b1059a6d4ae6a81857cdcfb577b0
Anifa <anifabazubagira@gmail.com>
2025-10-21 05:54:50 -0700

    Update solution - CodeHub

--- 8-kyu/switch-it-up/switch-it-up.java
+++ 8-kyu/switch-it-up/switch-it-up.java
@@ -1,27 +1,28 @@
+ 
 public class Kata
 {
   public static String switchItUp(int number)
   {
     switch (number){
-        case number==1;
+        case number==1
         return "One";
         break;
-        case number==2;
+        case number==2
         return "Two";
         break;
-        case number==3;
+        case number==3
         return "Three";
         break;
-        case number==4;
+        case number==4
         return "Four";
         break;
-        case number==5;
+        case number==5
         return "Five";
         break;
-        case number==6;
+        case number==6
         return "Six";
         break;
-        case number==7;
+        case number==7
         return "Seven";
         break;
         default:
