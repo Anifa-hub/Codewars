8a3931040958f4073a9e19f5a82fc7886ced3c89
Anifa <anifabazubagira@gmail.com>
2025-10-24 14:57:27 +0200

    Update solution - CodeHub

--- 8-kyu/twice-as-old/twice-as-old.java
+++ 8-kyu/twice-as-old/twice-as-old.java
@@ -1,10 +1,9 @@
- 
 public class TwiceAsOld {
 ​
     public static int twiceAsOld(int dadYears, int sonYears) {
         //TODO: Add code here
         return (dadYears -
-                sonYears *2);
+                (-sonYears *2);
     }
 ​
 }
