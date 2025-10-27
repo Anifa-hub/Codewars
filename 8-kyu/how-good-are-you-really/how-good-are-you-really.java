6c9e5812fc410f3e8aca7b9851d491fdf53057f7
Anifa <anifabazubagira@gmail.com>
2025-10-27 18:23:08 +0200

    Update solution - CodeHub

--- 8-kyu/how-good-are-you-really/how-good-are-you-really.java
+++ 8-kyu/how-good-are-you-really/how-good-are-you-really.java
@@ -1,21 +1,13 @@
-public class Solution {
+public class Kata {
     public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
-        // Calculate the sum of all class points
         double sumClassPoints = 0;
         for (int score : classPoints) {
             sumClassPoints += score;
         }
-​
-        // Add your points to the sum
         sumClassPoints += yourPoints;
-​
-        // Calculate the total number of students, including yourself
         int totalStudents = classPoints.length + 1;
-​
-        // Calculate the average score
         double averageScore = sumClassPoints / totalStudents;
-​
-        // Compare your score with the average
         return yourPoints > averageScore;
     }
+​
 }
