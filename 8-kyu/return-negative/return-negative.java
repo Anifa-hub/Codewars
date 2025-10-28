ab97dcb5220cee649fb62b89091680ac5048ad14
Anifa <anifabazubagira@gmail.com>
2025-10-28 15:20:53 +0200

    Update solution - CodeHub

--- 8-kyu/return-negative/return-negative.java
+++ 8-kyu/return-negative/return-negative.java
@@ -2,19 +2,9 @@ public class Kata {
 ​
   public static int makeNegative(final int x) {
     
-    switch(x){
-        case -x:
-        return x;
-        break;
-        case 0:
-        return 0;
-        break;
-        default:
-        return -x;
-        break;
-    }
+   return -Math.abs(x);
     
-    return ; // Your code here.
+    
     
   }
   
