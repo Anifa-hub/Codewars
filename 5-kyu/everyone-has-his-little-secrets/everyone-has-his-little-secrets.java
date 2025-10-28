56af7b20779a84b56c8476af3f205ca17b67bfa2
Anifa <anifabazubagira@gmail.com>
2025-10-28 15:34:01 +0200

    Update solution - CodeHub

--- 5-kyu/everyone-has-his-little-secrets/everyone-has-his-little-secrets.java
+++ 5-kyu/everyone-has-his-little-secrets/everyone-has-his-little-secrets.java
@@ -1,6 +1,7 @@
+ 
 public class LittleClass{
-  public static void isMySecret(String secret){
-    System.out.println("my secret is "+ secret);
+  public static String isMySecret(String secret){
+    return secret;
   }
 }
 ​
