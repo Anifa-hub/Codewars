0cb2da88a7c719cbc3721e797028d3a1e8c6c420
Anifa <anifabazubagira@gmail.com>
2025-10-28 15:29:53 +0200

    Update solution - CodeHub

--- 5-kyu/everyone-has-his-little-secrets/everyone-has-his-little-secrets.java
+++ 5-kyu/everyone-has-his-little-secrets/everyone-has-his-little-secrets.java
@@ -1,6 +1,8 @@
  
 public class Guesser {
-  
+   public void isMySecret(String secret){
+     System.out.println("my secret is "+ secret);
+   }
     public void guess() {
      return   LittleClass.isMySecret("thatILoveUnicorns");
     }
