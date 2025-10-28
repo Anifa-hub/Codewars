21135c20050dd4c0feb940b8481a35667e9d2496
Anifa <anifabazubagira@gmail.com>
2025-10-28 15:03:17 +0200

    Add solution - CodeHub

---
+++ 8-kyu/cat-years-dog-years/cat-years-dog-years.java
@@ -0,0 +1,23 @@
+public class Dinglemouse {
+​
+    public static int[] humanYearsCatYearsDogYears(final int humanYears) {
+      
+        int catYears;
+        int dogYears;
+​
+        if (humanYears == 1) {
+            catYears = 15;
+            dogYears = 15;
+            
+        } else if (humanYears == 2) {
+            catYears = 24;
+            dogYears = 24;
+            
+        } else {int yearsAfterTwo = humanYears - 2;
+            catYears = 24 + (yearsAfterTwo * 4);
+            dogYears = 24 + (yearsAfterTwo * 5);
+        }
+        
+        return new int[]{humanYears, catYears, dogYears};
+    }
+}
