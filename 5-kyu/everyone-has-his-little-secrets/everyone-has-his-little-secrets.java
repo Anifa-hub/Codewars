e5edd9d3cedfafff8ca5ded385734df50f085e7d
Anifa <anifabazubagira@gmail.com>
2025-10-28 15:38:54 +0200

    Update solution - CodeHub

--- 5-kyu/everyone-has-his-little-secrets/everyone-has-his-little-secrets.java
+++ 5-kyu/everyone-has-his-little-secrets/everyone-has-his-little-secrets.java
@@ -1,23 +1,15 @@
  
-​
-// File 1 (Conceptually): LittleClass.java
-public class LittleClass {
-    
-    // The method must be static because Guesser calls it using the class name.
-    // It returns the String provided by Guesser, which is the "secret."
+// Submit this code if the conflict is with 'LittleClass'
+public class TheLittleClass { // <--- Renamed
     public static String isMySecret(String secret) {
         return secret;
     }
 }
-// ------------------------------------------------------------------------
-// File 2 (Conceptually): Guesser.java
+​
 public class Guesser {
-    
-    // This is the class and method the tests are calling.
     public void guess() {
-        // This call is now valid because LittleClass is a top-level class 
-        // and isMySecret is a public static method.
-        LittleClass.isMySecret("thatILoveUnicorns");
+        TheLittleClass.isMySecret("thatILoveUnicorns"); // <--- Reference updated
     }
+​
 }
 ​
