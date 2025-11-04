04a4ad24f1e97dede9e7f4937c62b643a92d1660
Anifa <anifabazubagira@gmail.com>
2025-11-04 16:23:22 +0200

    Update solution - CodeHub

--- 7-kyu/max-diff-easy-1/max-diff-easy-1.java
+++ 7-kyu/max-diff-easy-1/max-diff-easy-1.java
@@ -5,7 +5,7 @@ public class Kata {
   public static int maxDiff(int[] lst) {
     //int [] fin = Arrays.toString(Arrays.sort(lst));
     Arrays.sort(lst);
-    int min = lst[0];
+    int min = lst[1];
     int max = lst[lst.length-1];
     return max-min;
   }
