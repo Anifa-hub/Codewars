f18905f9084d5a8aeadd045930e69c0b1f262e48
Anifa <anifabazubagira@gmail.com>
2025-11-11 11:23:12 +0200

    Update solution - CodeHub

--- 6-kyu/sort-the-odd/sort-the-odd.java
+++ 6-kyu/sort-the-odd/sort-the-odd.java
@@ -1,11 +1,23 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+​
 public class Kata {
   public static int[] sortArray(int[] array) {
-    for(int i= 0; i < array.length; i++){
-      if(array[i] %2 !=0){
-       System.out.println( Arrays.toString(Arrays.sort(array)));
+    List<Integer> oddNumbers = new ArrayList<>();
+    for (int number : array) {
+      if (number % 2 != 0) {
+        oddNumbers.add(number);
+      }
+    }
+    Collections.sort(oddNumbers);
+​
+    int oddIndex = 0;
+    for (int i = 0; i < array.length; i++) {
+        array[i] = oddNumbers.get(oddIndex);
+        oddIndex++;
       }
     }
     return array;
   }
-}
