0a90b7174795ff194c77df7393953669998863c4
Anifa <anifabazubagira@gmail.com>
2025-11-11 11:16:38 +0200

    Update solution - CodeHub

--- 6-kyu/sort-the-odd/sort-the-odd.java
+++ 6-kyu/sort-the-odd/sort-the-odd.java
@@ -1,9 +1,10 @@
+ 
 import java.util.Arrays;
 public class Kata {
   public static int[] sortArray(int[] array) {
     for(int i= 0; i < array.length; i++){
       if(array[i] %2 !=0){
-       return Arrays.sort(array);
+       return Arrays.toString(Arrays.sort(array));
       }
     }
     return array;
