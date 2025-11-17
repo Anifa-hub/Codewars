9d1f4ca9c8fe4fbde0b72b345cd5c6439d5c2fdd
Anifa <anifabazubagira@gmail.com>
2025-11-17 11:41:37 +0200

    Update solution - CodeHub

--- 7-kyu/sorted-yes-no-how/sorted-yes-no-how.java
+++ 7-kyu/sorted-yes-no-how/sorted-yes-no-how.java
@@ -1,11 +1,28 @@
 import java.util.*;
+​
 class Solution {
     public static String isSortedAndHow(int[] array) {
-      if(array.equals(Arrays.sort(array))){
-      return  "yes, ascending";
-      } else if(Arrays.sort(array,Comparator.reverseOrder())){
-        return "yes, descending";
-      }
-       return "no"; 
+        boolean ascending = true;
+        boolean descending = true;
+​
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i+1]) {
+                ascending = false;
+            }
+            if (array[i] < array[i+1]) {
+                descending = false;
+            }
+            // Optional optimization
+            if (!ascending && !descending) {
+                return "no";
+            }
+        }
+​
+        if (ascending) {
+            return "yes, ascending";
+        } else if (descending) {
+            return "yes, descending";
+        }
+        return "no"; 
     }
 }
