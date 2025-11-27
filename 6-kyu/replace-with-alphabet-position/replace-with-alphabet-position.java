d319b441b17d7c126a322e19604cb870cf0e92a7
Anifa <anifabazubagira@gmail.com>
2025-11-27 15:51:26 +0200

    Add solution - CodeHub

---
+++ 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.java
@@ -0,0 +1,12 @@
+ import java.util.stream.Collectors;
+class Kata {
+     static String alphabetPosition(String text) {
+        return text.chars()                     
+                   .filter(Character::isLetter) 
+                   .map(Character::toLowerCase) 
+                   .map(c -> c - 'a' + 1)       
+                   .mapToObj(String::valueOf)   
+                   .collect(Collectors.joining(" ")); 
+    }
+}
+​
